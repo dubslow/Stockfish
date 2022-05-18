16deade120629d60bf2dbe058cbd3e7ee1682bfe
Dubslow <bunslow@gmail.com>
2022-05-18 13:59:25 -0500

    SE scale depth: try vaguely-log depth bound
    (should be easy STC passer)
    
    bench 5379282

--- src/search.cpp
+++ src/search.cpp
@@ -1063,10 +1063,17 @@ moves_loop: // When in check, search starts here
           // then that move is singular and should be extended. To verify this we do
           // a reduced search on all the other moves but the ttMove and if the
           // result is lower than ttValue minus a margin, then we will extend the ttMove.
-          int depthBoundDiscount = std::min(thisThread->previousDepth / 7, 4); // At higher depths, do more SEs
+
+          int depthBound =   thisThread->previousDepth <= 1  ? 8 // At higher depths, looser bound = more SEs
+                           : thisThread->previousDepth <= 3  ? 7
+                           : thisThread->previousDepth <= 6  ? 6
+                           : thisThread->previousDepth <= 13 ? 5
+                           : thisThread->previousDepth <= 26 ? 4
+                           : thisThread->previousDepth <= 53 ? 3
+                           :                                   2;
 
           if (   !rootNode
-              &&  depth >= 6 - depthBoundDiscount + 2 * (PvNode && tte->is_pv())
+              &&  depth >= depthBound + 2 * (PvNode && tte->is_pv())
               &&  move == ttMove
               && !excludedMove // Avoid recursive singular search
            /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
