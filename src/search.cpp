e8177314e9a769dc990223c8682ba3abeea6db9f
Dubslow <bunslow@gmail.com>
2022-05-22 06:06:00 -0500

    take 3: reversed (just to see what happens)
    bench 6599529

--- src/search.cpp
+++ src/search.cpp
@@ -1195,9 +1195,9 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
-          int pvDepthBound =   thisThread->previousDepth <= 18 ? 6
+          int pvDepthBound =   thisThread->previousDepth <= 18 ? 3
                              : thisThread->previousDepth <= 27 ? 4
-                             :                                   3;
+                             :                                   5;
           int deeper =   r >= -1                   ? 0
                        : moveCount <= 4            ? 2
                        : PvNode && depth > pvDepthBound ? 1
