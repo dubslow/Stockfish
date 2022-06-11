839aefddf4c307b8a34ec7160835e0b693bb5ec3
Dubslow <bunslow@gmail.com>
2022-06-10 19:42:59 -0500

    LMR complex pos: try adding two yellows tweaked
    bench 6541883

--- src/search.cpp
+++ src/search.cpp
@@ -1175,7 +1175,7 @@ moves_loop: // When in check, search starts here
               // Decrease reduction for PvNodes based on depth
               r -= 1 + 15 / ( 3 + depth );
               // Decrease reduction at PvNodes according to complexity and eval gap
-              r -= complexity / 625 + abs(ss->staticEval - bestValue) / 250;
+              r -= complexity / 550 + abs(ss->staticEval - bestValue) / 100;
           }
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           else if ((ss+1)->cutoffCnt > 3)
