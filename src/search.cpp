2b626f54a4a80821e0decb60fa35214716743cb1
Dubslow <bunslow@gmail.com>
2022-06-14 12:16:17 -0500

    Singular beta crazy STC tune 50%
    bench 4265229

--- src/search.cpp
+++ src/search.cpp
@@ -517,11 +517,6 @@ void Thread::search() {
 
 namespace {
 
-              int C1=2, PL1=-32, PD1=-16, C2=20, PL2=-64, PD2=-32, T2=5;
-              TUNE(SetRange(-1000,1000), PL1, PD1, PL2, PD2);
-              TUNE(SetRange(-100,100), C2);
-              TUNE(SetRange(-20,20), C1, T2);
-
   // search<>() is the main search function for both PV and non-PV nodes
 
   template <NodeType nodeType>
@@ -1077,6 +1072,7 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
+              int C1=6, PL1=145, PD1=-60, C2=-1, PL2=15, PD2=-75, T2=4;
               int slope    =    tte->is_pv() + C1 + (PL1*ss->ply)/512 + (PD1*thisThread->previousDepth)/512;
               int constant = T2*tte->is_pv() + C2 + (PL2*ss->ply)/512 + (PD2*thisThread->previousDepth)/512;
               Value singularBeta = ttValue - slope * depth + constant;
@@ -1396,6 +1392,7 @@ moves_loop: // When in check, search starts here
 
     return bestValue;
   }
+
 
   // qsearch() is the quiescence search function, which is called by the main search
   // function with zero depth, or recursively with further decreasing depth per call.
