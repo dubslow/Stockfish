a576584f24cbbf71baf876d6db1ced39da062a52
Dubslow <bunslow@gmail.com>
2022-06-15 17:34:53 -0500

    Try removing deeper altogether (should fail right?)
    bench 4094367

--- src/search.cpp
+++ src/search.cpp
@@ -1187,12 +1187,7 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
-          // In general we want to cap the LMR depth search at newDepth. But if reductions
-          // are really negative and movecount is low, we allow this move to be searched
-          // deeper than the first move (this may lead to hidden double extensions).
-          int deeper =   r >= -1 ? 0 : 1;
-
-          Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
+          Depth d = std::clamp(newDepth - r, 1, newDepth);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
