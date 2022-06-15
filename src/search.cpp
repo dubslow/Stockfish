47bb6dfd1e07fda95ecb937e67e8deca78eec772
Dubslow <bunslow@gmail.com>
2022-06-15 17:30:32 -0500

    Try flat deeper 1
    bench 4826759

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
-          int deeper =   r >= -1 ? 0 : 2;
+          int deeper =   r >= -1 ? 0 : 1;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
