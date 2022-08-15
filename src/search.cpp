9e3d85d959e12099f21576044692a2581232c6b1
Dubslow <bunslow@gmail.com>
2022-08-15 03:37:45 -0500

    (19% tune) antirazoring, pv && d<=7 && (3a+253b)/256<ev<b --> ev=qs(ev-240-2d, b), return failhighs
    bench 5836254

--- src/search.cpp
+++ src/search.cpp
@@ -516,7 +516,7 @@ void Thread::search() {
                 skill.best ? skill.best : skill.pick_best(multiPV)));
 }
 
-int D=7, AB=115, EC=-220, ES=-14;
+int D=7, AB=125, EC=-240, ES=-2;
 
 namespace {
 
