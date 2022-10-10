8c5848587c34b98c350463c5ecce5449268b2aeb
Dubslow <bunslow@gmail.com>
2022-10-10 04:30:52 -0500

    antirazor: d<=7 && ev<min(beta-400+100*d, b) --> qs<NonPV>(b-1,b)
    (this is probably insane?)
    
    bench 2399443

--- src/search.cpp
+++ src/search.cpp
@@ -782,10 +782,10 @@ namespace {
             return value;
     }
     // Check if eval can quickly fail high
-    else if (   PvNode && depth <= 14
-             && (alpha + 3*beta)/4 < eval && eval < beta)
+    else if (   depth <= 7
+             && eval < std::min(beta - 400 + 100 * depth, beta))
     {
-        value = qsearch<PV>(pos, ss, beta-1, beta);
+        value = qsearch<NonPV>(pos, ss, beta-1, beta);
         if (value >= beta)
             return value;
     }
