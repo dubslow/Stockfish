5e9b0fea42264b3560de8e52c1582ce8af411945
Dubslow <bunslow@gmail.com>
2022-10-21 06:23:48 -0500

    fut prune remove !ttpv
    bench 4385573

--- src/search.cpp
+++ src/search.cpp
@@ -784,8 +784,7 @@ namespace {
 
     // Step 8. Futility pruning: child node (~25 Elo).
     // The depth condition is important for mate finding.
-    if (   !ss->ttPv
-        &&  depth < 8
+    if (    depth < 8
         &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 303 >= beta
         &&  eval >= beta
         &&  eval < 28031) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
