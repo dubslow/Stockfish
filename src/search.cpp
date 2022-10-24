a9e74eac540a34e6b89e9e98344cb8d1d064cb9d
Dubslow <bunslow@gmail.com>
2022-10-24 05:24:55 -0500

    if (a < ev < b && c < 40-2d^2) return qs(a,b)
    bench 4559186

--- src/search.cpp
+++ src/search.cpp
@@ -781,8 +781,8 @@ namespace {
         if (value < alpha)
             return value;
     }
-    else if (alpha < eval && eval < beta && complexity < 40 - 2*depth*depth)
-        return eval;
+    else if (alpha < eval && eval < beta && complexity < 60 - 2*depth*depth)
+        return qsearch<NonPV>(pos, ss, alpha, beta);
     // Step 8. Futility pruning: child node (~25 Elo).
     // The depth condition is important for mate finding.
     else if (   eval >= beta
