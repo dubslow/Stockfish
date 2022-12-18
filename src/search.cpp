a994158c111bd54e4fa1844910e223938f5738be
Dubslow <bunslow@gmail.com>
2022-12-18 17:41:51 -0600

    if (v < sb - dext^2/4) ext=2
    bench 3964161

--- src/search.cpp
+++ src/search.cpp
@@ -1065,8 +1065,7 @@ moves_loop: // When in check, search starts here
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
-                      && value < singularBeta - 25
-                      && ss->doubleExtensions <= 10)
+                      && value < singularBeta - (ss->doubleExtensions*ss->doubleExtensions)/4)
                   {
                       extension = 2;
                       depth += depth < 12;
