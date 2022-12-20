dfd5289faaa6a944d7e243d78e181d08b1820442
Dubslow <bunslow@gmail.com>
2022-12-20 16:39:22 -0600

    steal V1's idea: s18b min(mND, nD - (v > a && d > 6))
    bench 2915307

--- src/search.cpp
+++ src/search.cpp
@@ -1224,7 +1224,7 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv[0] = MOVE_NONE;
 
           value = -search<PV>(pos, ss+1, -beta, -alpha,
-                              std::min(maxNextDepth, newDepth - (value > alpha && newDepth > 1)), false);
+                              std::min(maxNextDepth, newDepth - (value > alpha && depth > 6)), false);
       }
 
       // Step 19. Undo move
