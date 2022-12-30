f140748990df06479c30c4f31968df7ffebd2ecd
Dubslow <bunslow@gmail.com>
2022-12-29 18:51:32 -0600

    LMR if (ss+1cC > 3) r += 2 - (ss+1mC > 6)
    bench 3306489

--- src/search.cpp
+++ src/search.cpp
@@ -1155,7 +1155,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction if next ply has a lot of fail high
       if ((ss+1)->cutoffCnt > 3)
-          r++;
+          r += 2 - ((ss+1)->moveCount > 6);
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
