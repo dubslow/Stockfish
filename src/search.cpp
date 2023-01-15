58fde92b5c7f2e3f6de2b33d2fde8d9249d495c8
Dubslow <bunslow@gmail.com>
2023-01-15 05:49:12 -0600

    else if remove depth condition
    bench 3588057

--- src/search.cpp
+++ src/search.cpp
@@ -1360,11 +1360,11 @@ moves_loop: // When in check, search starts here
                          quietsSearched, quietCount, capturesSearched, captureCount, depth);
 
     // Bonus for prior countermove that caused the fail low
-    else if (   (depth >= 5 || PvNode || bestValue < alpha - 65 * depth)
+    else if (   (PvNode || bestValue < alpha - 65 * depth)
              && !priorCapture)
     {
         // Extra bonuses for PV/Cut nodes or bad fail lows
-        int bonus = 2 + (bestValue < alpha - 88 * depth);
+        int bonus = 1 + (PvNode || cutNode) + (bestValue < alpha - 88 * depth);
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * bonus);
     }
 
