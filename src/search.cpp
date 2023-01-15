67ba08401740a31a70c7ecc5bbe35426bdccaf6c
Dubslow <bunslow@gmail.com>
2023-01-15 05:19:44 -0600

    if (nonSing && ttv >= b) ext = -2 - !(sscC > 2)
    bench 3739480

--- src/search.cpp
+++ src/search.cpp
@@ -1086,7 +1086,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2 + !predictNonSing;
+                  extension = -2 - !predictNonSing;
 
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
