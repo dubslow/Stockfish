898ab1a51a27650518b0aa810e116d15e6147d48
Dubslow <bunslow@gmail.com>
2023-01-15 05:36:37 -0600

    dDS &&= ply < rootD*2; dEDS &&= dDS;
    bench 4311195

--- src/search.cpp
+++ src/search.cpp
@@ -1191,8 +1191,8 @@ moves_loop: // When in check, search starts here
           {
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
-              const bool doDeeperSearch = value > (alpha + 66 + 11 * (newDepth - d));
-              const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5 && ss->ply < thisThread->rootDepth * 2;
+              const bool doDeeperSearch = ss->ply < thisThread->rootDepth * 2 && value > (alpha + 66 + 11 * (newDepth - d));
+              const bool doEvenDeeperSearch = doDeeperSearch && value > alpha + 582 && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
