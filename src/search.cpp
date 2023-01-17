c3fbde4da79a3cb857c2610a217fa4a5f00caabd
Dubslow <bunslow@gmail.com>
2023-01-17 17:01:53 -0600

    extensions if (ply < rootDepth*3)
    bench 3864315

--- src/search.cpp
+++ src/search.cpp
@@ -1037,7 +1037,7 @@ moves_loop: // When in check, search starts here
 
       // Step 15. Extensions (~100 Elo)
       // We take care to not overdo to avoid search getting stuck.
-      if (ss->ply < thisThread->rootDepth * 2)
+      if (ss->ply < thisThread->rootDepth * 3)
       {
           // Singular extension search (~94 Elo). If all moves but one fail low on a
           // search of (alpha-s, beta-s), and just one fails high on (alpha, beta),
