ef02d21289a6c9ddccc8f780a9c1c07661687acc
Dubslow <bunslow@gmail.com>
2023-01-17 18:08:07 -0600

    remove LMR fail bonus
    bench 3795217

--- src/search.cpp
+++ src/search.cpp
@@ -1201,14 +1201,6 @@ moves_loop: // When in check, search starts here
 
               if (newDepth > d)
                   value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
-
-              int bonus = value > alpha ?  stat_bonus(newDepth)
-                                        : -stat_bonus(newDepth);
-
-              if (capture)
-                  bonus /= 6;
-
-              update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
       }
 
