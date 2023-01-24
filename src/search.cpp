64ba073a000262582066e18942fd1d741e3bf92a
Dubslow <bunslow@gmail.com>
2023-01-24 07:12:33 -0600

    stat_bonus 300 * d - 280 ceiling 2000
    bench 3916439

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min(350 * d - 400, 2000);
+    return std::min(300 * d - 280, 2000);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
