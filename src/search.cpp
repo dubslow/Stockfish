94cf235f04e77f8f13007e1a2e96822222d1ab14
Dubslow <bunslow@gmail.com>
2023-01-26 09:21:13 -0600

    singext &&= !pv || ttv + 50 + 10 * ttd > a
    bench 4676360

--- src/search.cpp
+++ src/search.cpp
@@ -1063,7 +1063,7 @@ moves_loop: // When in check, search starts here
               &&  abs(ttValue) < VALUE_KNOWN_WIN
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3
-              &&  ttValue + 250 * tte->depth() > alpha)
+              && (!PvNode || ttValue + 50 + 10 * tte->depth() > alpha))
           {
               Value singularBeta = ttValue - (3 + (ss->ttPv && !PvNode)) * depth;
               Depth singularDepth = (depth - 1) / 2;
