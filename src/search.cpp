2a4f9ef18d3d389c17055c29b3fa54b590ac2b7d
Dubslow <bunslow@gmail.com>
2023-01-27 11:24:02 -0600

    [rebased] moveloop futprune if (eval + 103 + 136lmrd - imprvmt/14 < a)
    bench 4022564

--- src/search.cpp
+++ src/search.cpp
@@ -1028,7 +1028,7 @@ moves_loop: // When in check, search starts here
               // Futility pruning: parent node (~13 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 13
-                  && ss->staticEval + 103 + 136 * lmrDepth <= alpha)
+                  && ss->staticEval + 103 + 136 * lmrDepth - improvement / 14 <= alpha)
                   continue;
 
               lmrDepth = std::max(lmrDepth, 0);
