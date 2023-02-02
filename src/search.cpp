15988df44ddb8957eff1b67453c26421aa41d5dc
Dubslow <bunslow@gmail.com>
2023-02-02 13:25:32 -0600

    qs null static tempo bonus +200
    bench 4442408

--- src/search.cpp
+++ src/search.cpp
@@ -1479,10 +1479,10 @@ moves_loop: // When in check, search starts here
                 bestValue = ttValue;
         }
         else
-            // In case of null move search use previous static eval with a different sign
+            // In case of null move search use previous static eval with a different sign plus tempo bonus
             ss->staticEval = bestValue =
             (ss-1)->currentMove != MOVE_NULL ? evaluate(pos)
-                                             : -(ss-1)->staticEval;
+                                             : -(ss-1)->staticEval + 200;
 
         // Stand pat. Return immediately if static value is at least beta
         if (bestValue >= beta)
