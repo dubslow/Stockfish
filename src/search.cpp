ed7c4c5c358dd33cc070d9a47c3ae814ec5c9c24
Dubslow <bunslow@gmail.com>
2023-02-03 11:11:57 -0600

    excluded: use tte-no-static, skip ttcutoffs, skip ev=ttv
    bench 3873688

--- src/search.cpp
+++ src/search.cpp
@@ -737,6 +737,7 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (    ttValue != VALUE_NONE
+            && !excludedMove
             && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
             eval = ttValue;
     }
