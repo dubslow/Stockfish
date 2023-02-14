f9a3e23191c6473800e5777600f915422de2ba8c
Dubslow <bunslow@gmail.com>
2023-02-14 17:53:56 -0600

    singDblExt whether or not pv
    bench 6398100

--- src/search.cpp
+++ src/search.cpp
@@ -1076,8 +1076,7 @@ moves_loop: // When in check, search starts here
                   singularQuietLMR = !ttCapture;
 
                   // Avoid search explosion by limiting the number of double extensions
-                  if (  !PvNode
-                      && value < singularBeta - 25
+                  if (   value < singularBeta - 25
                       && ss->doubleExtensions <= 10)
                   {
                       extension = 2;
