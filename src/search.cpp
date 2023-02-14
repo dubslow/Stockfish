690275494ca3b0b10ae1b7873bee40855da76b51
Dubslow <bunslow@gmail.com>
2023-02-14 16:18:27 -0600

    singDblExt if ((!pv || ((sB in b±50)))
    bench 5182800

--- src/search.cpp
+++ src/search.cpp
@@ -1078,7 +1078,7 @@ moves_loop: // When in check, search starts here
                   singularQuietLMR = !ttCapture;
 
                   // Avoid search explosion by limiting the number of double extensions
-                  if (  !PvNode
+                  if (   (!PvNode || (beta - 50 < singularBeta && singularBeta < beta + 50))
                       && value < singularBeta - 25
                       && ss->doubleExtensions <= 10)
                   {
