2a388bf4eebc178de9e2b80e7a23dd114d9be338
Dubslow <bunslow@gmail.com>
2023-02-19 00:23:24 -0600

    b = pv|cut + bv<a-88d + bv<a-100-170d
    bench 3389052

--- src/search.cpp
+++ src/search.cpp
@@ -1374,7 +1374,7 @@ moves_loop: // When in check, search starts here
     else if (!priorCapture)
     {
         // Extra bonuses for PV/Cut nodes or bad fail lows
-        int bonus = (depth > 4) + (PvNode || cutNode) + (bestValue < alpha - 88 * depth);
+        int bonus = (PvNode || cutNode) + (bestValue < alpha - 88 * depth) + (bestValue < alpha - 100 - 170 * depth);
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * bonus);
     }
 
