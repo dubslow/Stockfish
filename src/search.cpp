51fd5ca517c51be872adc171e9f3808636758b9b
Dubslow <bunslow@gmail.com>
2023-02-24 05:26:52 -0600

    checkext &&= abs(ttv) > 88
    bench 4863649

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,8 @@ moves_loop: // When in check, search starts here
           // Check extensions (~1 Elo)
           else if (   givesCheck
                    && depth > 10
-                   && abs(ss->staticEval) > 88)
+                   && abs(ss->staticEval) > 88
+                   && (ttValue != VALUE_NONE && abs(ttValue) > 88))
               extension = 1;
 
           // Quiet ttMove extensions (~1 Elo)
