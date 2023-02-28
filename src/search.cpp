57713410eb313d58aedb037e757908f37f183063
Dubslow <bunslow@gmail.com>
2023-02-27 23:04:38 -0600

    nonsing ext -= ttv < a
    bench 4350157

--- src/search.cpp
+++ src/search.cpp
@@ -1108,7 +1108,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
-                  extension = -1;
+                  extension = -1 - (ttValue < alpha);
           }
 
           // Check extensions (~1 Elo)
