3181e42575d32fdf9b2eead4a1ae1a869ab9a558
Dubslow <bunslow@gmail.com>
2023-03-06 17:04:39 -0600

    nonsing x-=1 except ttv<=a [vs PR]
    bench 4244435

--- src/search.cpp
+++ src/search.cpp
@@ -1104,11 +1104,11 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2;
+                  extension = -3;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
-                  extension = -1;
+                  extension = -2;
 
               // If the eval of ttMove is less than alpha, we reduce it (negative extension)
               else if (ttValue <= alpha)
