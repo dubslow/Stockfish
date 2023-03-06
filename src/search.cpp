52cdd0cce76e01002f2ad46c587e7bd73bee6f0f
Dubslow <bunslow@gmail.com>
2023-03-06 17:04:39 -0600

    ttv<=v -=1 [vs PR]
    bench 4492182

--- src/search.cpp
+++ src/search.cpp
@@ -1108,7 +1108,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
-                  extension = -1;
+                  extension = -2;
 
               // If the eval of ttMove is less than alpha, we reduce it (negative extension)
               else if (ttValue <= alpha)
