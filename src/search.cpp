88cec5a8c0f9d19eaa9ee491ba97041355cb7cfc
Dubslow <bunslow@gmail.com>
2023-03-06 17:04:39 -0600

    all nonsing ext -= 1 [vs PR]
    bench 4369245

--- src/search.cpp
+++ src/search.cpp
@@ -1104,15 +1104,15 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2;
+                  extension = -3;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
-                  extension = -1;
+                  extension = -2;
 
               // If the eval of ttMove is less than alpha, we reduce it (negative extension)
               else if (ttValue <= alpha)
-                  extension = -1;
+                  extension = -2;
           }
 
           // Check extensions (~1 Elo)
