392b7514d07c302da7f4f3b1d9fa1ca5bba2bd08
Dubslow <bunslow@gmail.com>
2023-03-13 08:49:06 -0500

    (ttv<=v) x -= !pv; (ttv<=a) x = -2 * !pv
    bench 4294370

--- src/search.cpp
+++ src/search.cpp
@@ -1108,11 +1108,11 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
-                  extension = -1;
+                  extension = -1 - !PvNode;
 
               // If the eval of ttMove is less than alpha, we reduce it (negative extension)
               else if (ttValue <= alpha)
-                  extension = -1;
+                  extension = -2 * !PvNode;
           }
 
           // Check extensions (~1 Elo)
