8bd89f8e40990df2761dc79f33424c5d9003eb9e
Dubslow <bunslow@gmail.com>
2023-04-07 04:31:37 -0500

    "fixed" qs nochecks -3, recapts -7
    bench 4146742

--- src/types.h
+++ src/types.h
@@ -225,13 +225,13 @@ enum : int {
   // The basic stage is searching all captures+checks, then we omit checks,
   // and finally we search only *re*captures on the previous square.
   DEPTH_QS_ALL        =  0,
-  DEPTH_QS_NO_CHECKS  = -1,
+  DEPTH_QS_NO_CHECKS  = -3,
   DEPTH_QS_RECAPTURES = DEPTH_QS_NO_CHECKS - 4,
 
   // NONE is used everywhere as the nonvalue,
   // and OFFSET is used only in TT entry occupancy check (see f7b3f0e)
-  DEPTH_NONE   = DEPTH_QS_RECAPTURES - 1, // -6
-  DEPTH_OFFSET = DEPTH_NONE - 1 // -7
+  DEPTH_NONE   = DEPTH_QS_RECAPTURES - 1, // -8
+  DEPTH_OFFSET = DEPTH_NONE - 1 // -9
 };
 
 enum Square : int {
