5375f53ca443f1bed755ca93c96e6afa5fa747b9
Dubslow <bunslow@gmail.com>
2023-04-14 17:47:28 -0500

    sB margin 6 - 3*(cD > 18)
    bench 3626986

--- src/search.cpp
+++ src/search.cpp
@@ -1083,7 +1083,7 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - (3 + 2 * (ss->ttPv && !PvNode)) * depth / 2;
+              Value singularBeta = ttValue - (6 - 3 * (thisThread->completedDepth > 18) + 2 * (ss->ttPv && !PvNode)) * depth / 2;
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
