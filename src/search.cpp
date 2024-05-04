ead45069793c6e0cc6eb602728c45e0f42d67489
Dubslow <bunslow@gmail.com>
2024-05-04 13:03:47 -0500

    d += 9
    bench 2319536

--- src/search.cpp
+++ src/search.cpp
@@ -1055,8 +1055,6 @@ moves_loop:  // When in check, search starts here
                         extension    = 1 + (value < singularBeta - doubleMargin) 
                                          + (value < singularBeta - tripleMargin)
                                          + (value < singularBeta - quadMargin);
-
-                        depth += ((!PvNode) && (depth < 14));
                 }
 
                 // Multi-cut pruning
