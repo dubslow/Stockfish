53f0d9387d939fd1a416d6a0535b082afcbc01a7
Dubslow <bunslow@gmail.com>
2024-05-12 22:23:04 -0500

    upd qhists pawn *2/4
    bench 1461795

--- src/search.cpp
+++ src/search.cpp
@@ -1810,7 +1810,7 @@ void update_quiet_histories(
     update_continuation_histories(ss, pos.moved_piece(move), move.to_sq(), bonus);
 
     int pIndex = pawn_structure_index(pos);
-    workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()] << bonus;
+    workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()] << (2 * bonus) / 4;
 }
 
 // Updates move sorting heuristics
