2dbc321cc60f3a7ecf859e792f20bc6b63a70396
Dubslow <bunslow@gmail.com>
2024-05-15 19:28:25 -0500

    mvpick capt 2*hist (kek)
    bench 1145074

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -168,7 +168,7 @@ void MovePicker::score() {
         if constexpr (Type == CAPTURES)
             m.value =
               7 * int(PieceValue[pos.piece_on(m.to_sq())])
-              + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
+              + 2 * (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
 
         else if constexpr (Type == QUIETS)
         {
