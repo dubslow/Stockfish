cd1b0aea5905acf10f457d5dffd200856b15d982
Dubslow <bunslow@gmail.com>
2024-05-20 00:14:13 -0400

    delete lmrContHist + (QS_CHECKS, razoring, recapt exts)
    bench 1191976

--- src/search.cpp
+++ src/search.cpp
@@ -1158,13 +1158,6 @@ moves_loop:  // When in check, search starts here
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
-
-                // Post LMR continuation history updates (~1 Elo)
-                int bonus = value <= alpha ? -stat_malus(newDepth)
-                          : value >= beta  ? stat_bonus(newDepth)
-                                           : 0;
-
-                update_continuation_histories(ss, movedPiece, move.to_sq(), bonus);
             }
         }
 
