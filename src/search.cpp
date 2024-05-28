80c24a493321f0469683fcf3d5556b95d641047e
Dubslow <bunslow@gmail.com>
2024-05-28 17:55:17 -0500

    faillow capthist tune 39% iters
    bench 1169141

--- src/search.cpp
+++ src/search.cpp
@@ -515,6 +515,9 @@ void Search::Worker::clear() {
     refreshTable.clear(networks[numaAccessToken]);
 }
 
+int B1=88, B2=110, B3=319, B4=158, B5=65, B6=174;
+int P2=16000, P3=183, P4=22;
+
 
 // Main search function for both PV and non-PV nodes.
 template<NodeType nodeType>
@@ -1337,21 +1340,32 @@ moves_loop:  // When in check, search starts here
                          quietCount, capturesSearched, captureCount, depth);
 
     // Bonus for prior countermove that caused the fail low
-    else if (!priorCapture && prevSq != SQ_NONE)
+    else if (prevSq != SQ_NONE)
     {
+      if (!priorCapture)
+      {
         int bonus = (116 * (depth > 5) + 115 * (PvNode || cutNode)
                      + 186 * ((ss - 1)->statScore < -14144) + 121 * ((ss - 1)->moveCount > 9)
                      + 64 * (!ss->inCheck && bestValue <= ss->staticEval - 115)
                      + 137 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 81));
+
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
                                       stat_bonus(depth) * bonus / 100);
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()]
           << stat_bonus(depth) * bonus / 200;
-
-
         if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
             thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
               << stat_bonus(depth) * bonus / 25;
+      }
+      else
+      {
+        int bonus = (B1 * (depth > 5) + B2 * (PvNode || cutNode)
+                     + B3 * ((ss - 1)->statScore < -P2) + B4 * ((ss - 1)->moveCount > 9)
+                     + B5 * (!ss->inCheck && bestValue <= ss->staticEval - P3)
+                     + B6 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - P4));
+
+        thisThread->captureHistory[pos.piece_on(prevSq)][prevSq][pos.captured_piece()] << bonus;
+      }
     }
 
     if (PvNode)
