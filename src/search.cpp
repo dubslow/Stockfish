6d8e03f5ad1c12496d4949dfdf180c172f2efdf1
FauziAkram <fauzi.dabat@hotmail.com>
2024-05-29 04:20:46 +0300

    delete pcRetVal + lmr(Dds+ContHist) + (QS_CHECKS, razoring, recapt exts)
    bench 1197537

--- src/search.cpp
+++ src/search.cpp
@@ -881,8 +881,7 @@ Value Search::Worker::search(
                     // Save ProbCut data into transposition table
                     tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3,
                               move, unadjustedStaticEval, tt.generation());
-                    return std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY ? value - (probCutBeta - beta)
-                                                                     : value;
+                    return probCutBeta;
                 }
             }
 
