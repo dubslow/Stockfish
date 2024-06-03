23b442ae30bcf703a2a5b8636a5dd94711e6125a
Dubslow <bunslow@gmail.com>
2024-06-02 20:21:26 -0500

    faillow capthist fixed (no ss/seval) tune 20%
    bench 1259814

--- src/search.cpp
+++ src/search.cpp
@@ -515,12 +515,8 @@ void Search::Worker::clear() {
     refreshTable.clear(networks[numaAccessToken]);
 }
 
-int C=0, B1=334, B2=343, B3=343, B4=869;
+int C=-15, B1=599, B2=352, B3=244, B4=1016;
 int P1=5, P4=9;
-auto BRange = [](int b){return std::pair<int, int>(-b*6, b*6);};
-TUNE(SetRange(BRange), C, B1, B2, B3, B4);
-TUNE(SetRange(1, 20), P1);
-TUNE(SetRange(4, 16), P4);
 
 // Main search function for both PV and non-PV nodes.
 template<NodeType nodeType>
