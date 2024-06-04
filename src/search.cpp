94d45d177a0ea63a5506801d09c506cba1239447
Dubslow <bunslow@gmail.com>
2024-06-04 18:25:39 -0500

    fauzi ss-4 rep r--
    bench 1414235

--- src/search.cpp
+++ src/search.cpp
@@ -1151,6 +1151,10 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r++;
 
+        if (   move == (ss-4)->currentMove
+          && pos.has_repeated())
+            r--;
+
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
             r++;
