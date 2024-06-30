c27ca55128cf507ac584c5f8ed17d64946243a9f
Dubslow <bunslow@gmail.com>
2024-06-30 15:11:44 -0400

    probc ttsv (0v+4b)/4
    bench 1174935

--- src/search.cpp
+++ src/search.cpp
@@ -890,11 +890,15 @@ Value Search::Worker::search(
 
                 if (value >= probCutBeta)
                 {
+                    if (std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
+                    {
+                        value = beta + 1;
+                    }
                     // Save ProbCut data into transposition table
                     ttWriter.write(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER,
                                    depth - 3, move, unadjustedStaticEval, tt.generation());
-                    return std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY ? value - (probCutBeta - beta)
-                                                                     : value;
+
+                    return value;
                 }
             }
 
