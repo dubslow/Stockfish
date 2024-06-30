4c3c8289748958c0ad7349c9409185a42643e1a9
Dubslow <bunslow@gmail.com>
2024-06-30 08:21:43 -0400

    mvc > 2 + !ttm
    bench 1008649

--- src/search.cpp
+++ src/search.cpp
@@ -1565,7 +1565,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
             if (!givesCheck && move.to_sq() != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
                 && move.type_of() != PROMOTION)
             {
-                if (moveCount > 2)
+                if (moveCount > 2 + !bool(ttData.move))
                     continue;
 
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
