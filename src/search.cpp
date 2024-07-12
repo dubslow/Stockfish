d10b35cf54bfff97767361d7943c303f257db064
Dubslow <bunslow@gmail.com>
2024-07-12 17:25:05 -0400

    (3b+nv)/4
    bench 1316887

--- src/search.cpp
+++ src/search.cpp
@@ -817,7 +817,7 @@ Value Search::Worker::search(
         if (nullValue >= beta && nullValue < VALUE_TB_WIN_IN_MAX_PLY)
         {
             if (thisThread->nmpMinPly || depth < 16)
-                return nullValue;
+                return (3 * beta + nullValue) / 4;
 
             assert(!thisThread->nmpMinPly);  // Recursive verification is not allowed
 
@@ -830,7 +830,7 @@ Value Search::Worker::search(
             thisThread->nmpMinPly = 0;
 
             if (v >= beta)
-                return nullValue;
+                return (3 * beta + nullValue) / 4;
         }
     }
 
