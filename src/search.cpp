5584bd46af3a924c4b87ddb90be4e780798ddadc
Linmiao Xu <linmiao.xu@gmail.com>
2026-04-11 17:14:47 -0400

    [spec ltc 69daba8a3ca80bdf151d47f1] ch update in qs
    bench 2931445

--- src/search.cpp
+++ src/search.cpp
@@ -1753,6 +1753,16 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
     if (!is_decisive(bestValue) && bestValue > beta)
         bestValue = (bestValue + beta) / 2;
 
+    if (!ss->inCheck && !(bestMove && pos.capture(bestMove))
+        && ss->staticEval != VALUE_NONE
+        && (bestValue > ss->staticEval) == bool(bestMove))
+    {
+      auto bonus = std::clamp(int(bestValue - ss->staticEval) / 8,
+                              -CORRECTION_HISTORY_LIMIT / 4,
+                              CORRECTION_HISTORY_LIMIT / 4);
+      update_correction_history(pos, ss, *this, bonus / 2);
+    }
+
     // Save gathered info in transposition table. The static evaluation
     // is saved as it was before adjustment by correction history.
     ttWriter.write(posKey, value_to_tt(bestValue, ss->ply), pvHit,
