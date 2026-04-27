1c4937d5dd975a1c89c1c303839fa5f449dc2670
Dubslow <bunslow@gmail.com>
2026-04-27 15:50:59 -0400

    [h/16] cluster=256 kekw
    bench 3172249

--- src/tt.cpp
+++ src/tt.cpp
@@ -132,14 +132,14 @@ void TTWriter::write(
 // of TTEntry. Each non-empty TTEntry contains information on exactly one position. The size of a Cluster should
 // divide the size of a cache line for best performance, as the cacheline is prefetched when possible.
 
-static constexpr int ClusterSize = 3;
+static constexpr int ClusterSize = 25;
 
 struct Cluster {
     TTEntry entry[ClusterSize];
-    char    padding[2];  // Pad to 32 bytes
+    char    padding[6];  // Pad to 256 bytes (kekw)
 };
 
-static_assert(sizeof(Cluster) == 32, "Suboptimal Cluster size");
+static_assert(sizeof(Cluster) == 256, "Suboptimal Cluster size");
 
 
 // Sets the size of the transposition table,
