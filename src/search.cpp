6756a11b60b98a88b9042ec2dabc7cb6638beced
Dubslow <bunslow@gmail.com>
2026-06-27 05:58:56 -0500

    small pc !inC
    bench 2131562

--- src/search.cpp
+++ src/search.cpp
@@ -1078,13 +1078,13 @@ Value Search::Worker::search(
         }
     }
 
-moves_loop:  // When in check, search starts here
-
     // Step 12. A small Probcut idea
     probCutBeta = beta + 428;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
+
+moves_loop:  // When in check, search starts here
 
     const PieceToHistory* contHist[] = {
       (ss - 1)->continuationHistory, (ss - 2)->continuationHistory, (ss - 3)->continuationHistory,
