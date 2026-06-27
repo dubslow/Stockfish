914fd5bc92b43c3269458a84adab79869059b45a
Dubslow <bunslow@gmail.com>
2026-06-27 05:49:33 -0500

    ttpen small-pc either mismatch
    bench 2684748

--- src/search.cpp
+++ src/search.cpp
@@ -1082,9 +1082,13 @@ moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
     probCutBeta = beta + 428;
-    if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
-        && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
-        return probCutBeta;
+    if (ttData.depth >= depth - 4 && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
+    {
+        if ((ttData.bound & BOUND_LOWER) && ttData.value >= probCutBeta)
+            return probCutBeta;
+        else if (ttData.bound & (ttData.value >= probCutBeta ? BOUND_UPPER : BOUND_LOWER))
+            ttWriter.penalize(1);
+    }
 
     const PieceToHistory* contHist[] = {
       (ss - 1)->continuationHistory, (ss - 2)->continuationHistory, (ss - 3)->continuationHistory,
