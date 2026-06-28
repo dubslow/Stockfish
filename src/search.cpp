4834898d2bb4ba07db6b49ee2f3150c4af45b718
Dubslow <bunslow@gmail.com>
2026-06-28 07:14:59 -0500

    spc !pv, -=100
    bench 2573136

--- src/search.cpp
+++ src/search.cpp
@@ -1081,8 +1081,8 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
-    probCutBeta = beta + 428;
-    if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
+    probCutBeta = beta + 328;
+    if (!PvNode && (ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
 
