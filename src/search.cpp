19b27189d5c14217bdee42dd5f46a2e11b8ce659
Dubslow <bunslow@gmail.com>
2026-07-01 16:10:09 -0500

    avg += (prevFH - 2) * d / 8
    bench 2384253

--- src/search.cpp
+++ src/search.cpp
@@ -280,7 +280,7 @@ bool Search::Worker::iterative_deepening() {
     Value  bestValue     = -VALUE_INFINITE;
     Color  us            = rootPos.side_to_move();
     double timeReduction = 1, totBestMoveChanges = 0;
-    int    delta, iterIdx                        = 0;
+    int    delta, prevFailHighs = 0, iterIdx = 0;
 
     // Allocate stack with extra size to allow access from (ss - 7) to (ss + 2):
     // (ss - 7) is needed for update_continuation_histories(ss - 1) which accesses (ss - 6),
@@ -373,7 +373,7 @@ bool Search::Worker::iterative_deepening() {
 
             // Reset aspiration window starting size
             delta     = 5 + threadIdx % 8 + std::abs(rootMoves[pvIdx].meanSquaredScore) / 10193;
-            Value avg = rootMoves[pvIdx].averageScore;
+            Value avg = rootMoves[pvIdx].averageScore + (prevFailHighs - 2) * delta / 8;
             alpha     = std::max(avg - delta, -VALUE_INFINITE);
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
@@ -499,6 +499,8 @@ bool Search::Worker::iterative_deepening() {
 
             if (threads.stop)
                 break;
+
+            prevFailHighs = failedHighCnt;
         }
 
         const bool forgottenMate = lastBestMoveScore != -VALUE_INFINITE
