e977b0350752a1261accba0bf94998877e3dfd9a
Dubslow <bunslow@gmail.com>
2026-08-16 23:35:45 -0400

    pseudo-cancel the constants, surprised the bench doesn't change tbh
    bench 2666650

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -76,7 +76,7 @@ Value scale_evaluation(Value nnue, int optimism, const Position& pos) {
     // Conversely, in hard positions, the optimism boost is minimized.
     // To maintain overall evaluation scale, the static NNUE score is dampened proportionally.
     // At average alignment of 2047, the optimism boost is around 1.5x.
-    int base_eval = nnue - (i64(nnue) * alignment) / 131072 + ((optimism + (optimism * alignment) / 4096) * 7674) / 90649;
+    int base_eval = nnue - (i64(nnue) * alignment) / 131072 + ((optimism + (optimism * alignment) / 4096) * 16) / 189;
 
     // 4. Scale the combined evaluation by material volume.
     // Higher material on the board amplifies the final evaluation magnitude.
